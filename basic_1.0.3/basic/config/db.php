2e3c559144381b6f0703d447ccf38f00d79ef18a
lei <dengleitju@163.com>
2015-03-31 15:36:41 +0800

    delete v

--- basic_1.0.3/basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=dcmd',
-    'username' => 'dcmd',
-    'password' => 'dcmd123456',
-    'charset' => 'utf8',
-];
--- basic_1.0.3/basic/config/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'pagesize' => 20,
-    'timeZone' => 'Asia/Chongqing',
-];
